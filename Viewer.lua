e4669e9eedada8bdf427a716abbe7cfeef08f8db
Narrev <NarrevRBX@gmail.com>
2016-02-24 18:46:37 -0600

    Update Viewer.lua

--- Viewer.lua
+++ Viewer.lua
@@ -1,8 +1,8 @@
-GetTimeFromSeconds = function(seconds)
-	--- Return hours, minutes, and seconds from seconds since 1970
-	local hours	= floor(seconds / 3600 % 24)
-	local minutes	= floor(seconds / 60 % 60)
-	local seconds	= floor(seconds % 60)
-	
+GetTimeFromSeconds = function(unix)
+	--- Return hours, minutes, and seconds from unix
+	local hours	= floor(unix / 3600 % 24)
+	local minutes	= floor(unix / 60 % 60)
+	local seconds	= floor(unix % 60)
+
 	return hours, minutes, seconds
 end
