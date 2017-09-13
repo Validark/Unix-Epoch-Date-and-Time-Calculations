e23ec96d6d1e1c3d5f5c3b8285c0258b44371730
Validark <NarrevRBX@gmail.com>
2017-09-13 09:39:30 -0600

    Update Archived Functions.lua

--- Archived Functions.lua
+++ Archived Functions.lua
@@ -3,12 +3,12 @@
 -- Discovered by Narrev before I learned about calculus
 
 local function GetLeaps(year)
-        --- Returns the number of Leap days in a given amount of years
+	--- Returns the number of Leap days in a given amount of years
 	return floor(year/4) - floor(year/100) + floor(year/400)
 end
 
 local function CountDays(year)
-        --- Returns the number of days in a given number of years
+	--- Returns the number of days in a given number of years
 	return 365*year + GetLeaps(year)
 end
 
@@ -18,7 +18,7 @@ local function CountDays2(year)
 end
 
 local function overflow(array, seed)
-        --- Subtracts the integer values in an array from a seed until the seed cannot be subtracted from any further (aka "overflow")
+	--- Subtracts the integer values in an array from a seed until the seed cannot be subtracted from any further (aka "overflow")
 	-- @param array array A table filled with integers to be subtracted from seed
 	-- @param integer seed A seed that is subtracted by the values in array until it would become negative from subtraction
 	-- @returns index at which the iterated value is greater than the remaining seed and remainder of seed (before subtracting final value)
@@ -31,9 +31,9 @@ local function overflow(array, seed)
 	end
 end
 
-local function GetYMDFromSeconds(seconds) -- Archived
-        --- Returns the Year, Month, and Days, from seconds since 1970 (not recommended)
-        -- @param number seconds The amount of seconds since January 1st, 1970
+local function GetYMDFromSeconds(seconds) -- Archived (not as efficient)
+	--- Returns the Year, Month, and Days, from seconds since 1970
+	-- @param number seconds The amount of seconds since January 1st, 1970
         
 	-- 86400 is the number of seconds per day
 	
@@ -45,29 +45,28 @@ local function GetYMDFromSeconds(seconds) -- Archived
 	--			  floor(seconds / 86400) + CountDays(1970) + 1
 	-- Doesn't matter which way you do it. But one of them should help you understand what is happening to the numbers.
 	
-        local days              = ceil((seconds + 1) / 86400) + CountDays(1970)
-        
-        -- Each of the following caculations gets the next bracket of years we are in
-        -- It's similar to converting from base x to base 10, except the number's places are worth 400, 100, 4, 1
-        local _400Years         = 400*floor(days / CountDays(400))
-        local _100Years         = 100*floor(days % CountDays(400) / CountDays(100))
-        local _4Years           =   4*floor(days % CountDays(400) % CountDays(100) / CountDays(4))
-        
-        -- Declare local variables
-        local _1Years, month
-        
-        -- days is number days into the current year (this would be considered "ydays" in os.date) 
-        _1Years, days		= overflow({366,365,365,365}, days - CountDays2(_4Years + _100Years + _400Years)) -- [0-1461]
-        
-        
-        -- We subtract 1 because function overflow returns 1 if you are already in the correct year
-	_1Years			= _1Years - 1
-        
-        -- Add the year brackets together, get the year
-        local year		= _1Years + _4Years + _100Years + _400Years
-        
-        -- Now find the month and number of days into the month
-        month, days	        = overflow({31,isLeapYear(year) and 29 or 28,31,30,31,30,31,31,30,31,30,31}, days)
-        
+	local days = ceil((seconds + 1) / 86400) + CountDays(1970)
+	
+	-- Each of the following caculations gets the next bracket of years we are in
+	-- It's similar to converting from base x to base 10, except the number's places are worth 400, 100, 4, 1
+	local _400Years = 400*floor(days / CountDays(400))
+	local _100Years = 100*floor(days % CountDays(400) / CountDays(100))
+	local _4Years   =   4*floor(days % CountDays(400) % CountDays(100) / CountDays(4))
+	
+	-- Declare local variables
+	local _1Years, month
+	
+	-- days is number days into the current year (this would be considered "ydays" in os.date) 
+	_1Years, days = overflow({366,365,365,365}, days - CountDays2(_4Years + _100Years + _400Years)) -- [0-1461]
+	
+	-- We subtract 1 because function overflow returns 1 if you are already in the correct year
+	_1Years = _1Years - 1
+	
+	-- Add the year brackets together, get the year
+	local year = _1Years + _4Years + _100Years + _400Years
+	
+	-- Now find the month and number of days into the month
+	month, days = overflow({31,isLeapYear(year) and 29 or 28,31,30,31,30,31,31,30,31,30,31}, days)
+	
 	return year, month, days
 end
